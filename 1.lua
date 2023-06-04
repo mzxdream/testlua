eceec0799cf20f9219700ff4433f8a362c13f95a
mzxdream <mzxdream@gmail.com>
2023-06-04 16:43:54 +0800

    1

--- 1.lua
+++ 1.lua
@@ -1,5 +1,3 @@
-local lpeg = require("lpeg")
-
 local function dump(o)
     if type(o) == 'table' then
         local s = '{ '
@@ -15,6 +13,7 @@ local function dump(o)
     end
 end
 
+local lpeg = require("lpeg")
 -- match
 local peg_sign_match = lpeg.S("+-") ^ -1
 local peg_hex_match = lpeg.P("0") * lpeg.S("xX") * lpeg.R("09", "af", "AF") ^ 1
@@ -23,54 +22,33 @@ local peg_int_match = peg_sign_match * (peg_hex_match + peg_dec_match)
 local peg_float_match = peg_sign_match * peg_dec_match * (lpeg.P(".") * lpeg.R("09") ^ 1) ^ -1 * (lpeg.S("eE") * peg_sign_match * peg_dec_match) ^ -1
 local peg_num_match = peg_sign_match * (peg_hex_match + peg_dec_match * (lpeg.P(".") * lpeg.R("09") ^ 1) ^ -1 * (lpeg.S("eE") * peg_sign_match * peg_dec_match) ^ -1)
 local peg_space_match = lpeg.S(" \n\t") ^ 0
-local peg_letter_match = lpeg.R("az", "AZ") ^ 1
-local peg_func_match = lpeg.P("min") + lpeg.P("max") + lpeg.P("ceil") + lpeg.P("floor")
+local peg_var_match = lpeg.R("az", "AZ", "__") * lpeg.R("az", "AZ", "__", "09") ^ 0
 
 local function PegSpaceWrap(pattern) -- " pattern "
     return peg_space_match * pattern * peg_space_match
 end
 
-local function PegTupleWrap(pattern) -- "pattern,pattern,pattern"
-    pattern = PegSpaceWrap(pattern)
-    return pattern * (',' * pattern)^0
-end
-
-local function PegOptPolishWrap(pattern)
-    return pattern / function (left, opt, right)
-        return opt, left, right
+local function PegOptWrap(pattern)
+    return pattern / function (opt, right)
+        return right, opt
     end
 end
 
 -- capture
 local peg_num_capture = PegSpaceWrap(peg_num_match / tonumber)
+local peg_var_capture = PegSpaceWrap(lpeg.C(peg_var_match))
 local peg_opt_term_capture = PegSpaceWrap(lpeg.C(lpeg.S("+-")))
 local peg_opt_factor_capture = PegSpaceWrap(lpeg.C(lpeg.S("*/%")))
 
---local peg_factor_parser = lpeg.Ct(PegOptPolishWrap(peg_num_capture * peg_opt_factor_capture * peg_num_capture))
-
---local peg_expression_parser = lpeg.P({
---    "exp",
---    exp = lpeg.Ct(lpeg.V("term") * (peg_opt_term_capture * lpeg.V("term")) ^ 0,
---    term = lpeg.
---    factor = lpeg.Ct(PegOptPolishWrap(peg_num_capture * peg_opt_factor_capture * (lpeg.V("factor") + peg_num_capture))),
---})
-
---local calculator = lpeg.P({
---  "exp",
---  exp = lpeg.V("factor") + integer,
---  factor = node(integer * muldiv * (lpeg.V("factor") + integer))
---})
---  Exp = lpeg.Ct(Term * (TermOp * Term)^0);
---  Term = lpeg.Ct(Factor * (FactorOp * Factor)^0);
---  Factor = Number + Open * Exp * Close;
-
 local peg_expression_parser = lpeg.P({
     "exp",
-    exp = lpeg.V("factor") + peg_num_capture,
-    factor = lpeg.Ct(PegOptPolishWrap(peg_num_capture * peg_opt_factor_capture * lpeg.V("exp"))),
+    exp = lpeg.V("term"),
+    term = lpeg.Ct(lpeg.V("factor") * PegOptWrap(peg_opt_term_capture * lpeg.V("factor")) ^ 1) + lpeg.V("factor"),
+    factor = lpeg.Ct(lpeg.V("basic") * PegOptWrap(peg_opt_factor_capture * lpeg.V("basic")) ^ 1) + lpeg.V("basic"),
+    basic = peg_num_capture + "(" * peg_space_match * lpeg.V("exp") * peg_space_match * ")",
 })
 
-local tt = {"1 * 2 + 1", "1234", "1234.6", " 12345 + 12345", "1 * 2 * 3 / 4 * 5 / 6 * 7"}
+local tt = {"1 * 2 + 1", "1 + 2 * 1", "3 * (1 + 2)", "1234", "1234.6", " 12345 + 12345", "1 + 2 + 3", " 1 * 2 * 3 ", "1 * 2 * 3 / 4 * 5 / 6 * 7"}
 for _, v in ipairs(tt) do
     local tmp = peg_expression_parser:match(v)
     print(v .. " -> " ..dump(tmp))
